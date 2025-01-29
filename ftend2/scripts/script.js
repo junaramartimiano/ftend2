e9223212f112fde5d3f4134973c59a5a75e1e256
junaramartimiano <160941079+junaramartimiano@users.noreply.github.com>
2025-01-29 08:55:33 -0300

    Delete ftend2 directory

--- ftend2/scripts/script.js
+++
@@ -1,7 +0,0 @@
-function menu(pagina) {
-    $('#conteudo').hide(2000,()=>{
-      $("#conteudo").load("../components/"+pagina+".html",()=>{
-         $('#conteudo').show(2000);
-      });   
-    });
-  }  
